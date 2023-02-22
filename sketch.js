219423c61fc8525cd42b7f8f668d9923f30dc537
GGuemb3l <126083608+GGuemb3l@users.noreply.github.com>
2023-02-22 16:46:25 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,9 +7,8 @@ function setup() {
   canvas.position(0,0);
   canvas.style('z-index', '-0');// changer de plan(arrière plan/avant plan)en dépendant de z
  image1=loadImage("Logo transparent 2.png")
-  //noCursor();
 
-//noCursor();
+noCursor();
 graphicpub = createGraphics(width, height);
 
 graphic = createGraphics(width, height); //taille du calque
@@ -31,7 +30,7 @@ if(mouseIsPressed){
  graphicpub.circle(mouseX,mouseY,40,40);
   }
 
- // image(image1,mouseX-30,mouseY-30,50,50);
+  image(image1,mouseX-30,mouseY-30,50,50);
 
 image(graphic,0,0); //affiche le graphique
 image(graphicpub,0,0);
